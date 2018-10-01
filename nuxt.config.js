20427d092031f2111316dc8f3386347054a47cfa
yamanoku <0910yama@gmail.com>
2018-10-01 15:47:37 +0900

    fix config

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,12 +23,6 @@ module.exports = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(vue)$/,
-          loader: 'vue-loader',
-          exclude: /(node_modules)/
-        })
       }
     }
   },
