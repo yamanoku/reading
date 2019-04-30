7b9d34757127508bfcf39a3d83d08f399052a70d
yamanoku <0910yama@gmail.com>
2019-04-30 11:55:43 +0900

    feat:async await

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,29 @@
 <template>
   <main>
-    <news-list
-      :data="lists"
-      :success="shown"
-      :error="modal" />
+    <news-list :data="lists"/>
   </main>
 </template>
 
 <script>
 import axios from "axios";
 import { TOKEN } from "~/static/config.js";
-import NewsList from '~/components/NewsList.vue'
+import NewsList from "~/components/NewsList.vue";
 
 export default {
-  asyncData() {
-    return axios
-      .get(TOKEN)
-      .then(res => {
-        return {
-          lists: res.data.api,
-          shown: true,
-          modal: false,
-        }
-      })
-      .catch(() => {
-        return {
-          lists: [],
-          shown: false,
-          modal: true,
-        }
-      });
+  async asyncData({ params, error }) {
+    try {
+      const { data } = await axios.get(TOKEN);
+      return {
+        lists: data.api
+      };
+    } catch {
+      error({ statusCode: 404, message: "Connection Error" });
+    }
   },
   components: {
     NewsList
   }
-}
+};
 </script>
 
 <style>
