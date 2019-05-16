3722961c4005882f60de26326a69bd9b5584806b
yamanoku <0910yama@gmail.com>
2019-05-16 01:01:17 +0000

    chore: update [skip ci]

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const TOKEN = `https://ou8tljek8g.execute-api.us-east-1.amazonaws.com/reading/readingFunctions`
+export const TOKEN = `https://ou8tljek8g.execute-api.us-east-1.amazonaws.com/reading/readingFunctions`;
