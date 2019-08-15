454153c08ad26ae25a6406d78a54855f25e160f4
yamanoku <0910yama@gmail.com>
2019-08-15 15:40:30 +0900

    chore: update [skip ci]

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-export const TOKEN = `https://ou8tljek8g.execute-api.us-east-1.amazonaws.com/reading/readingFunctions`;
+export const TOKEN = 'https://ou8tljek8g.execute-api.us-east-1.amazonaws.com/reading/readingFunctions';
+
+export const ALGOLIA = {
+  appId: 'AIVUUL01R1',
+  apiKey: '6b2a8e367a44661523c9439535dd9d53'
+}
--- sw.js
+++ sw.js
@@ -6,10 +6,6 @@ workbox.precaching.precacheAndRoute([
     "revision": "d4b57d71915611cf5c7b0a253230f320"
   },
   {
-    "url": "/_nuxt/0f83378554cfc221d502.js",
-    "revision": "1493f90ef2306a06cb13ea67dd5e2b97"
-  },
-  {
     "url": "/_nuxt/29f2f55b6d68942cd0bb.js",
     "revision": "5eb53a5f5a86079534584d6ef558bdc1"
   },
@@ -18,16 +14,20 @@ workbox.precaching.precacheAndRoute([
     "revision": "b94139aec12205f82e10ce6cf42bd243"
   },
   {
-    "url": "/_nuxt/6391c3ebc9ba32a21577.js",
-    "revision": "1377f92f285a3467b92c5859cc2c1804"
+    "url": "/_nuxt/956c2bacf13070ce4ebd.js",
+    "revision": "0194f0e2ddfb24a20161a63422b388e8"
   },
   {
-    "url": "/_nuxt/a989c8ba40ca93d61994.js",
-    "revision": "b3ff9afdeda8305defc178d196ec62bc"
+    "url": "/_nuxt/a92836490cd1d5e18fb3.js",
+    "revision": "5b0fdb1506a5ad048a7bd7cfa664cca0"
   },
   {
     "url": "/_nuxt/bd7ed51ad1a6c211cffb.js",
     "revision": "28e8eabdcbed85b4186a6e4724246663"
+  },
+  {
+    "url": "/_nuxt/f3d1a817e15598f0fbb3.js",
+    "revision": "b3ff9afdeda8305defc178d196ec62bc"
   }
 ], {
   "cacheId": "reading",
