921903efc339e0b527f27c0961ed2b7ec82df10f
yamanoku <0910yama@gmail.com>
2019-08-15 15:39:58 +0900

    set htmllang ja

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   head: {
     title: 'READING...',
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
