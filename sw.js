70bd90d48f84d4525a83271597406c579c27cd68
yamanoku <0910yama@gmail.com>
2020-01-08 01:01:20 +0000

    chore: update [skip ci]

--- sw.js
+++ sw.js
@@ -2,32 +2,32 @@ importScripts('/_nuxt/workbox.dev.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/_nuxt/05117de538c3784d21f8.js",
+    "revision": "7244a17ac275e4ea427a1442219af250"
+  },
+  {
+    "url": "/_nuxt/06a5369e182b0547d1a0.js",
+    "revision": "26cd7b37d5e474c97a0caa874e1b4910"
+  },
+  {
     "url": "/_nuxt/154810f0c4982d63a207.js",
     "revision": "4d345c7125aa5c63813ec3aa3c0058d5"
   },
   {
-    "url": "/_nuxt/226cdc0e717b173548c2.js",
-    "revision": "14cbf7a40fc6494f90bd12f0af0bd0a3"
-  },
-  {
-    "url": "/_nuxt/24da8b0e6cd7ea4732e6.js",
-    "revision": "701e46f20bf6d543c358b88c66f6a0d8"
-  },
-  {
-    "url": "/_nuxt/34e7dbcdcf543e54c634.js",
-    "revision": "90741b259791d91ee2e87b99fb2c6222"
+    "url": "/_nuxt/6a33b27046c8127ded30.js",
+    "revision": "d60ed66431b7e2f1561eb625f2cd5533"
   },
   {
     "url": "/_nuxt/78355be58f944fd3796f.js",
     "revision": "079c49070adf0ac2c01417feffd84d18"
   },
   {
-    "url": "/_nuxt/b2ec796d55c6fd8a6feb.js",
-    "revision": "22252cd234589219fbef3d4e5fc476d3"
+    "url": "/_nuxt/7fe7327ab6db89212f09.js",
+    "revision": "573764dd52038e7227266763ad09eef8"
   },
   {
-    "url": "/_nuxt/d9d078e09f398241de49.js",
-    "revision": "2c657dad943ffec23d4607146884a80c"
+    "url": "/_nuxt/abb2ada493c85bdadbe9.js",
+    "revision": "cfdf07875096593d9b8712289b989f12"
   }
 ], {
   "cacheId": "reading",
