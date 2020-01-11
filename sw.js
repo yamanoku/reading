ffa017db034eeeacf34c00d02703df419ff53c5c
yamanoku <0910yama@gmail.com>
2020-01-11 10:01:26 +0000

    chore: update [skip ci]

--- sw.js
+++ sw.js
@@ -1,43 +1,32 @@
-importScripts('/_nuxt/workbox.dev.4c4f5ca6.js')
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js')
 
-workbox.precaching.precacheAndRoute([
-  {
-    "url": "/_nuxt/23ff12187650a7dbb228.js",
-    "revision": "e2b4f19d98799f19622e113f294e30cf"
-  },
-  {
-    "url": "/_nuxt/250a6b888b614144b68d.js",
-    "revision": "4c5c044823d8d4e8de7ab267ec719529"
-  },
-  {
-    "url": "/_nuxt/29ea04cb76344e920f02.js",
-    "revision": "254dc6b66fb7c90a7841b981a5b43131"
-  },
-  {
-    "url": "/_nuxt/6eeb8058b175d7c61270.js",
-    "revision": "258f31d2b5e6027e953d045ff5b4724c"
-  },
-  {
-    "url": "/_nuxt/99a4f4eff6911c804d55.js",
-    "revision": "07a217df67632d67d2e9d235adfd98c5"
-  },
-  {
-    "url": "/_nuxt/9f28e438f495f46e1750.js",
-    "revision": "ba9edd8b54c68d7b8e1942fcaec8f5fb"
-  },
-  {
-    "url": "/_nuxt/d1f354f9e1f234ea695f.js",
-    "revision": "b51deac7fa98e00a2cffa085d8852573"
-  }
-], {
-  "cacheId": "reading",
-  "directoryIndex": "/",
-  "cleanUrls": false
+// --------------------------------------------------
+// Configure
+// --------------------------------------------------
+
+// Set workbox config
+workbox.setConfig({
+  "debug": true
 })
 
-workbox.clientsClaim()
-workbox.skipWaiting()
+// Start controlling any existing clients as soon as it activates
+workbox.core.clientsClaim()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+// Skip over the SW waiting lifecycle stage
+workbox.core.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.precaching.cleanupOutdatedCaches()
+
+// --------------------------------------------------
+// Precaches
+// --------------------------------------------------
+
+// Precache assets
+
+// --------------------------------------------------
+// Runtime Caching
+// --------------------------------------------------
+
+// Register route handlers for runtimeCaching
+workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
