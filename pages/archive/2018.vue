133f27166c148e8abb9190169c9f463b895ccee4
yamanoku <0910yama@gmail.com>
2020-03-01 18:52:02 +0900

    separate components

--- pages/archive/2018.vue
+++ pages/archive/2018.vue
@@ -4,99 +4,21 @@
     <nav>
       <a href="/archive">/archive</a>
     </nav>
-    <form role="search">
-      <label>キーワード検索
-        <input v-model="filterKey" type="text" name="keywords" autocomplete="on" list="title">
-      </label>
-    </form>
-    <ul v-show="filterListItem.length > 0" class="list">
-      <template v-for="list in filterListItem">
-        <li :key="list.iid">
-          <time :datetime="unix2ymd (list.ts)">{{ unix2ymd (list.ts) }}</time>
-          <!-- title not response -->
-          <a
-            v-if="!list.attachments[0].title"
-            :href="urlRender(list.attachments[0].text)"
-            target="_blank"
-            rel="noopener"
-          >
-            {{ textRender(emoji(list.attachments[0].text)) }}
-          </a>
-          <!-- title response -->
-          <a
-            v-else
-            :href="list.attachments[0].title_link"
-            target="_blank"
-            rel="noopener"
-          >
-            {{ emoji(list.attachments[0].title) }}
-          </a>
-        </li>
-      </template>
-    </ul>
-    <div v-show="filterListItem.length === 0">
-      キーワードなし
-    </div>
+    <archive-list :data="lists" />
   </main>
 </template>
 
 <script>
-import emoji from 'node-emoji'
 import JSON from '../../static/reading2018.json'
-
-const textRender = (text) => {
-  if (text.includes('Reading... ')) {
-    const mystr = text.split('Reading... ').join('')
-    return mystr.replace(/^<[^>]h>|<[^>]*>$/g, '')
-  } else if (text.includes('Reading… ')) {
-    const mystr = text.split('Reading… ').join('')
-    return mystr.replace(/^<[^>]h>|<[^>]*>$/g, '')
-  }
-}
+import ArchiveList from '../../components/ArchiveList.vue'
 
 export default {
+  components: {
+    ArchiveList
+  },
   data () {
     return {
-      filterKey: '',
       lists: JSON
-    }
-  },
-  computed: {
-    filterListItem () {
-      return this.lists.filter((list) => {
-        const searchRegex = new RegExp(this.filterKey, 'i')
-        return (
-          searchRegex.test(list.attachments[0].title) ||
-          searchRegex.test(textRender(list.attachments[0].text))
-        )
-      })
-    }
-  },
-  methods: {
-    textRender (text) {
-      if (text.includes('Reading... ')) {
-        const mystr = text.split('Reading... ').join('')
-        return mystr.replace(/^<[^>]h>|<[^>]*>$/g, '')
-      } else if (text.includes('Reading… ')) {
-        const mystr = text.split('Reading… ').join('')
-        return mystr.replace(/^<[^>]h>|<[^>]*>$/g, '')
-      }
-    },
-    emoji (text) {
-      return emoji.emojify(text)
-    },
-    urlRender (text) {
-      if (text === null) {
-        return
-      }
-      return text.match(/ <([^\s]+)/)[1].slice(0, -1)
-    },
-    unix2ymd (intTime) {
-      const d = new Date(intTime * 1000)
-      const year = d.getFullYear()
-      const month = ('0' + (d.getMonth() + 1)).slice(-2)
-      const day = ('0' + d.getDate()).slice(-2)
-      return year + '-' + month + '-' + day
     }
   }
 }
@@ -104,27 +26,4 @@ export default {
 
 <style lang="scss">
 @import "yama-normalize";
-
-[role="search"] {
-  margin: calc(var(--rhythm) * 3) 0;
-}
-
-.list {
-  padding: 0;
-  margin: calc(var(--rhythm) * 3) 0;
-  & > * {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  & > * > * {
-    flex-grow: 1;
-    flex-basis: 100px;
-  }
-  & > * > :last-child {
-    flex-basis: 0;
-    flex-grow: 999;
-    min-width: calc(50% - 90px);
-  }
-}
-
 </style>
--- pages/archive/2019.vue
+++ pages/archive/2019.vue
@@ -4,81 +4,26 @@
     <nav>
       <a href="/archive">/archive</a>
     </nav>
-    <ul>
-      <template v-for="list in lists">
-        <li v-if="list.attachments" :key="list.iid">
-          <span
-            class="off-screen"
-          >{{ unix2ymd (list.ts) }}</span>
-          <!-- title not response -->
-          <a
-            v-if="!list.attachments[0].title"
-            :href="urlRender(list.attachments[0].text)"
-            target="_blank"
-            rel="noopener"
-          >
-            {{ emoji(textRender(list.attachments[0].text)) }}
-          </a>
-          <!-- title response -->
-          <a
-            v-else
-            :href="list.attachments[0].title_link"
-            target="_blank"
-            rel="noopener"
-          >
-            {{ emoji(list.attachments[0].title) }}
-          </a>
-        </li>
-      </template>
-    </ul>
+    <archive-list :data="lists" />
   </main>
 </template>
 
 <script>
-import emoji from 'node-emoji'
 import JSON from '../../static/reading2019.json'
+import ArchiveList from '../../components/ArchiveList.vue'
 
 export default {
+  components: {
+    ArchiveList
+  },
   data () {
     return {
       lists: JSON
-    }
-  },
-  computed: {
-    limitCount () {
-      return this.lists.slice(0, 1000)
-    }
-  },
-  methods: {
-    textRender (text) {
-      if (text.includes('Reading... ')) {
-        const mystr = text.split('Reading... ').join('')
-        return mystr.replace(/^<[^>]h>|<[^>]*>$/g, '')
-      } else if (text.includes('Reading… ')) {
-        const mystr = text.split('Reading… ').join('')
-        return mystr.replace(/^<[^>]h>|<[^>]*>$/g, '')
-      }
-    },
-    emoji (text) {
-      return emoji.emojify(text)
-    },
-    urlRender (text) {
-      if (text === null) {
-        return
-      }
-      return text.match(/ <([^\s]+)/)[1].slice(0, -1)
-    },
-    unix2ymd (intTime) {
-      const d = new Date(intTime * 1000)
-      const year = d.getFullYear()
-      const month = ('0' + (d.getMonth() + 1)).slice(-2)
-      const day = ('0' + d.getDate()).slice(-2)
-      return year + '-' + month + '-' + day
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 @import "yama-normalize";
 </style>
